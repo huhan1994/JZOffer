3505468b4531186d0e778bf0b8e9ffffcfa4f6ee
huhan1994 <695286966@qq.com>
2018-02-07 19:21:41 +0800

    18/02/07

---
+++ src/Problem05/Solution.java
@@ -0,0 +1,18 @@
+package Problem05;
+
+import java.util.ArrayList;
+
+//输入一个链表，从尾到头打印链表每个节点的值。
+public class Solution {
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+    }
+}
